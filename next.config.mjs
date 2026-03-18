026cffd06263df83fbe80f55dfb0bc4ac89bc75e
monnas69 <s@monaghan.biz>
2026-03-18 15:05:10 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  https://hjavfsgiynsgwncrzhjl.supabase.co
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqYXZmc2dpeW5zZ3duY3J6aGpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyNTY5NTEsImV4cCI6MjA4NzgzMjk1MX0.jTr48ylE_Au3y9_H34J497MveNiwv3N2ck5WrghQjp0
 };
 
 export default nextConfig;
